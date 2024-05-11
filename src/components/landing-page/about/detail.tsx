832f16b4cc4a3ba88b8a2ac6a792cdef74b2dc15
Shubham Sharma <76767061+Mobiance@users.noreply.github.com>
2024-05-11 20:56:07 +0530

    initial commit

---
+++ src/components/landing-page/about/detail.tsx
@@ -0,0 +1,36 @@
+const Detail = () => {
+    return (
+        <div className="py-20 flex justify-between">
+            <div>
+                <h1>About us</h1>
+                <h1 className="text-4xl font-semibold">OUR GREEN MISSION</h1>
+            </div>
+            <div className="w-[480px] text-lg text-justify pt-20">
+                <h1>At Leaf Healer, we're dedicated to helping individuals
+                    create and maintain healthy. vibrant gardens. Our goal is
+                    to make gardening accessible to all, regardless of
+                    experience level, through detailed educational content.</h1>
+                <h1 className="pt-5">Leaf Healer believes in the power of plants to enrich our
+                    lives and environment. Our website serves as a
+                    comprehensive guide for gardeners of all levels, offering
+                    insights into plant care, disease prevention, and the
+                    creation of eco-friendly gardens. From the beginner whc
+                    just learning the ropes to the experienced gardener
+                    looking to enhance their knowledge, our content covers
+                    wide range of topics. Whether you're dealing with a
+                    specific plant disease, seeking tips on soil management, c
+                    exploring sustainable gardening practices, Leaf Healer
+                    provides the resources you need. Our commitment to
+                    promoting a greener world is encapsulated in our missior
+                    statement: to empower individuals with the knowledge
+                    and
+                    tools necessary for creating and sustaining healthy
+                    gardens. By providing informative content, practical tips,
+                    and innovative solutions, we aim to inspire and support
+                    gardeners in their pursuit of a healthier, more vibrant
+                    environment.</h1>
+            </div>
+        </div>
+    )
+}
+export default Detail;
---
+++ src/components/landing-page/about/list.tsx
@@ -0,0 +1,31 @@
+const List = () => {
+    return (
+        <div className="py-20 flex gap-y-10 flex-col">
+            <div className="w-[360px] text-justify items-start">
+                <h1 className="text-2xl font-semibold">01. Identify Solutions</h1>
+                <p className="text-md pt-4">Learn how to spot common and rare
+                    plant diseases through symptoms.
+                    Understand what yellow leaves, spots,
+                    and other signals mean. Early detection
+                    is crucial for effective treatment.</p>
+            </div>
+            <div className="w-[360px] text-justify items-start">
+                <h1 className="text-2xl font-semibold">02. Disease Solution</h1>
+                <p className="text-md pt-4">Learn how to spot common and rare
+                    plant diseases through symptoms.
+                    Understand what yellow leaves, spots,
+                    and other signals mean. Early detection
+                    is crucial for effective treatment.</p>
+            </div>
+            <div className="w-[360px] text-justify items-start">
+                <h1 className="text-2xl font-semibold">03. Plant Selection</h1>
+                <p className="text-md pt-4">Learn how to spot common and rare
+                    plant diseases through symptoms.
+                    Understand what yellow leaves, spots,
+                    and other signals mean. Early detection
+                    is crucial for effective treatment.</p>
+            </div>
+        </div>
+    )
+}
+export default List;
